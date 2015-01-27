43946c156afdba894a5a3538d06a8a3518d3ee36
Rob Scott <rob.scott87@gmail.com>
2015-01-26 20:30:43 -0500

    using single blpop with multiple keys so it will work more than once

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var common      = require('./common')
+  , _           = require('lodash')
   , redis       = common.redis
   , DeviceModel = require('./models/device');
 
@@ -12,41 +13,36 @@ listenTo({
 
 function listenTo(channels) {
   for (var key in channels) {
-    continuousBlpop(key);
-    console.log('listening to '+key);
-  }
-
-  function continuousBlpop(key) {
-
-    var method = channels[key];
-    if (typeof method === 'undefined') {
+    if (typeof channels[key] === 'undefined') {
       throw new Error("Invalid method for key: "+key);
     }
-
-    redis.blpop(key, 0, function(err, data) {
-      if (err) { 
-        console.error('error from blpop on '+key+':', err);
-        return continuousBlpop(key);
-      }
-
-      console.log(key+' blpop data', data);
-
-      if (!Array.isArray(data) || typeof data[1] !== 'string') {
-        console.error('unexpected data format on '+key+':', data);
-        return continuousBlpop(key);
-      }
-
-      try {
-        var parsedData = JSON.parse(data[1]);
-      } catch (e) {
-        console.error('invalid JSON on '+key+':', data[1]);
-        return continuousBlpop(key);
-      }
-
-      method(parsedData, function(err) {
-        if (err) { console.error("Error calling method for key: "+key, err); }
-        return continuousBlpop(key);
-      });
-    });
   }
+
+  redis.blpop(_.keys(channels).concat([0]), function(err, data) {
+    if (err) { 
+      console.error('error from blpop on '+key+':', err);
+      return listenTo(channels);
+    }
+
+    console.log(key+' blpop data', data);
+
+    if (!Array.isArray(data) || typeof data[0] !== 'string' || typeof data[1] !== 'string') {
+      console.error('unexpected data format on '+key+':', data);
+      return listenTo(channels);
+    }
+
+    var method = channels[data[0]];
+
+    try {
+      var parsedData = JSON.parse(data[1]);
+    } catch (e) {
+      console.error('invalid JSON on '+key+':', data[1]);
+      return listenTo(channels);
+    }
+
+    method(parsedData, function(err) {
+      if (err) { console.error("Error calling method for key: "+key, err); }
+      return listenTo(channels);
+    });
+  });
 }
