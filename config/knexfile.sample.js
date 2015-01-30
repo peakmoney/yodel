0546f0a9d1112cec511ca45e216608cba0c0f6d1
Rob Scott <rob.scott87@gmail.com>
2015-01-29 21:20:37 -0500

    adding test environment to sample knexfile

--- config/knexfile.sample.js
+++ config/knexfile.sample.js
@@ -1,5 +1,13 @@
 module.exports = {
-  development: {
+  test: {
+    client: 'mysql',
+    connection: {
+      "user": "root"
+    , "password": ""
+    , "database": "yodel_test"
+    }
+  }
+, development: {
     client: 'mysql',
     connection: {
       "user": "root"
