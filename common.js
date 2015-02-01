d26673cafa7a6c8973fca61c261994c784914ba2
Rob Scott <rob.scott87@gmail.com>
2015-02-01 13:51:46 -0500

    publishing events to redis when a separate redis_events config is specified, removing lodash dependency because we weren't using it enough to justify it

--- common.js
+++ common.js
@@ -25,19 +25,7 @@ var config = common.config = function config(name, allowBlank) {
 };
 
 common.knex = require('knex')(config('knexfile'));
-
-var redisConfig = config('redis', true) || {};
-redisConfig.port = redisConfig.port || 6379;
-redisConfig.host = redisConfig.host || '127.0.0.1';
-// Don't want to overwrite any data in a database for another env
-if (env == 'test' && typeof redisConfig.database === 'undefined') {
-  redisConfig.database = 5;
-}
-
-common.redis = require("redis").createClient(redisConfig.port, redisConfig.host, redisConfig.options);
-if (!isNaN(redisConfig.database)) {
-  common.redis.select(redisConfig.database);
-}
+common.redis = newRedisClient('redis');
 
 common.notifyError = function(err) {
   console.error(err);
@@ -51,4 +39,34 @@ if (config('sentry', true)) {
     if (!(err instanceof Error)) { err = new Error(err); }
     ravenClient.captureError(err, callback);
   }
+}
+
+common.publishEvent = function(event, callback) {
+  console.log('Event: ', event);
+  return callback && callback();
+}
+
+if (config('redis_events', true)) {
+  var redis = newRedisClient('redis_events');
+  common.publishEvent = function(stats, callback) {
+    redis.publish("yodel:events", JSON.stringify(stats), callback);
+  }
+}
+
+function newRedisClient(configName) {
+  var rConfig = config(configName, true) || {};
+  rConfig.port = rConfig.port || 6379;
+  rConfig.host = rConfig.host || '127.0.0.1';
+
+  // Don't want to overwrite any data in a database for another env
+  if (env == 'test' && typeof rConfig.database === 'undefined') {
+    rConfig.database = 5;
+  }
+
+  var client = require("redis").createClient(
+    rConfig.port, rConfig.host, rConfig.options);
+
+  if (!isNaN(config.database)) { client.select(config.database); }
+
+  return client;
 }
