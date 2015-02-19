447dc8ecaba41d4729b08ef0bd321c4c62c2c635
Brian Bowden <brian@bitstomp.com>
2015-02-19 14:58:29 -0500

    Added migration for gcm_notification_user table

---
+++ migrations/20150219142415_create_gcm_notification_user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('gcm_notification_user', function(table) {
+      table.increments();
+      table.integer('user_id').unsigned().notNullable();
+      table.string('notification_key_name', 177).notNullable();
+      table.text('notification_key').notNullable();
+      table.datetime('created_at').notNullable();
+      table.datetime('updated_at').notNullable();
+      table.charset('utf8mb4');
+      table.collate('utf8mb4_unicode_ci');
+      table.unique(['user_id']);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('gcm_notification_user')
+  ]);
+};
