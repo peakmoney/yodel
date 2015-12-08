747e8b70bfb420573735406c0b6e03ba60740e97
David Hunt <davidpaulhunt@gmail.com>
2015-12-08 12:19:13 -0500

    added feedback listener, called it as FeedbackService() in app.js, cleaned up some formatting i/e commas and variable declarations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var cluster = require('cluster')
-  , program = require('commander')
-  , https   = require('https')
-  , Promise = require('bluebird');
+var cluster = require('cluster');
+var program = require('commander');
+var https   = require('https');
+var Promise = require('bluebird');
 
 
 program
@@ -13,8 +13,8 @@ if (program.environment) {
   process.env.NODE_ENV = program.environment;
 }
 
-var common     = require('./common')
-  , numWorkers = program.workers || require('os').cpus().length;
+var common     = require('./common');
+var numWorkers = program.workers || require('os').cpus().length;
 
 if (cluster.isMaster) {
   // Fork workers. One per CPU for maximum effectiveness
@@ -41,14 +41,18 @@ if (cluster.isMaster) {
   }
 
 } else {
-  var DeviceModel   = require('./lib/models/device')
-    , RedisListener = require('./lib/listeners/redis');
+  var DeviceModel   = require('./lib/models/device');
+  var RedisListener = require('./lib/listeners/redis');
+  var FeedbackService = require('./lib/models/feedback');
 
   RedisListener.listen({
-    'yodel:subscribe':   DeviceModel.subscribe
-  , 'yodel:unsubscribe': DeviceModel.unsubscribe
-  , 'yodel:notify':      DeviceModel.notify
+    'yodel:subscribe':   DeviceModel.subscribe,
+    'yodel:unsubscribe': DeviceModel.unsubscribe,
+    'yodel:notify':      DeviceModel.notify
   });
+
+  // call our feedback service once our redis listener is listening
+  FeedbackService();
 
   console.log('Listening to yodel:subscribe, yodel:unsubscribe, and yodel:notify');
 }
@@ -60,8 +64,8 @@ function ping() {
   var cfg = common.config('ping', true);
   https.get(cfg.run_url, function() {
     return Promise.props({
-      sql: common.knex('devices').max('id as max_id')
-    , rds: common.redis.incrAsync('yodel:ping')
+      sql: common.knex('devices').max('id as max_id'),
+      rds: common.redis.incrAsync('yodel:ping')
     }).then(function(testResults) {
       testResults.sql = testResults.sql[0].max_id;
 
