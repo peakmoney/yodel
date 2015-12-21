f4fb7d57f414dd6c03a4131e116eb225cb630d34
David Hunt <davidpaulhunt@gmail.com>
2015-12-21 15:15:47 -0500

    added device_test.js to start testing methods that other things rely on, formatted processApnFeedback to handle an array of devices, a single device, or time and device, added tests to feedback_test.js to ensure only the correct device is being removed after feedback and only one at a time

--- test/feedback_test.js
+++ test/feedback_test.js
@@ -8,17 +8,30 @@ describe('Feedback', function() {
 
   before(function() {
     return resetBatch().then(function() {
-      return common.knex('devices').insert({
+      return common.knex('devices').insert([{
         user_id: 40,
         token:  'abc123',
         platform: 2,
         created_at: new Date(2015, 10, 1),
         updated_at: new Date(2015, 10, 15)
-      });
+      },{
+        user_id: 41,
+        token:  '123abc',
+        platform: 2,
+        created_at: new Date(2015, 10, 1),
+        updated_at: new Date(2015, 10, 15)
+      }]);
     });
   });
 
   describe('processApnFeedback', function() {
+    it('2 ios users should exist', function() {
+      return common.knex('devices').where({platform: 2}).then(function(results) {
+        results.should.be.instanceof(Array);
+        results.should.have.lengthOf(2);
+      });
+    });
+
     it('user 40 should exist', function() {
       return common.knex('devices').where({user_id: 40, token: 'abc123'}).then(function(results) {
         results.should.be.instanceof(Array);
@@ -43,6 +56,36 @@ describe('Feedback', function() {
 
     it('should have unsubscribed user 40', function() {
       return common.knex('devices').where({user_id: 40, token: 'abc123'}).then(function(results) {
+        results.should.be.instanceof(Array);
+        results.should.have.lengthOf(0);
+      });
+    });
+
+    it('should have only unsubscribed user 40', function() {
+      return common.knex('devices').where({platform: 2}).then(function(results) {
+        results.should.be.instanceof(Array);
+        results.should.have.lengthOf(1);
+      });
+    });
+
+    it('should not break emit "feedback"', function(done) {
+      feedback.service.emit('feedback', []);
+      done();
+    });
+
+    it('emit "feedback" with 2 args', function(done) {
+      feedback.service.emit('feedback', new Date().getTime(), '123abc');
+      done();
+    });
+
+    it('should wait for Event delete_device:41', function() {
+      this.timeout(3500);
+      return (helpers.actionWatcher.waitForEvent('delete_device:41')).should.be.fulfilled();
+    });
+
+
+    it('should have unsubscribed user 41', function() {
+      return common.knex('devices').where({user_id: 41, token: '123abc'}).then(function(results) {
         results.should.be.instanceof(Array);
         results.should.have.lengthOf(0);
       });
