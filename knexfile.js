d9df00d50537b9cf40e9b6879aa4fd2bb9aaa890
Rob Scott <rob.scott87@gmail.com>
2016-06-06 17:52:48 -0400

    more travis specific debugging

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,8 @@
 
 const config = require('./config');
 
+console.log(`Using ${config.knex.connection} as DB credentials`);
+
 module.exports = {
   client: 'mysql',
   connection: config.knex.connection,
