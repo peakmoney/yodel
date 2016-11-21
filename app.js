13824a4abb1e9d1599b0079244e0f471944c4260
Brian Bowden <brian@spire.me>
2016-11-21 14:44:17 -0500

    Moving the core lib and root js to more eslint-friendly ES6 standards

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var cluster = require('cluster');
-var program = require('commander');
-var https = require('https');
-var Promise = require('bluebird');
+const cluster = require('cluster');
+const program = require('commander');
+const https = require('https');
+const Promise = require('bluebird');
 
 
 program
@@ -13,72 +13,79 @@ if (program.environment) {
   process.env.NODE_ENV = program.environment;
 }
 
-var common = require('./common');
-var numWorkers = program.workers || require('os').cpus().length;
+const common = require('./common');
+// eslint-disable-next-line
+const numWorkers = program.workers || require('os').cpus().length;
+
+// this will fail if there is nothing in the devices table
+// that may not be what everyone wants
+function ping() {
+  const cfg = common.config('ping', true);
+  https.get(cfg.run_url, () => {
+    return Promise.props({
+      sql: common.knex('devices').max('id as max_id'),
+      rds: common.redis.incrAsync('yodel:ping'),
+    }).then((testResults) => {
+      const results = testResults;
+      results.sql = testResults.sql[0].max_id;
+
+      let hasError = false;
+      results.forEach((key) => {
+        if (isNaN(results[key])) {
+          hasError = true;
+          console.error(`Value not a number: ${key}`);
+        } else if (results[key] < 1) {
+          hasError = true;
+          console.error(`Value less than 1: ${key}`);
+        }
+      });
+
+      return hasError ? null : https.get(cfg.complete_url);
+    }).catch(common.notifyError);
+  });
+}
+
 
 if (cluster.isMaster) {
   // Fork workers. One per CPU for maximum effectiveness
-  for (var i = 0; i < numWorkers; i++) {
+  for (let i = 0; i < numWorkers; i++) {
     cluster.fork();
   }
 
-  cluster.on('exit', function(deadWorker, code, signal) {
+  cluster.on('exit', (deadWorker) => {
     // Restart the worker
-    var worker = cluster.fork();
+    const worker = cluster.fork();
 
     // Note the process IDs
-    var newPID = worker.process.pid;
-    var oldPID = deadWorker.process.pid;
+    const newPID = worker.process.pid;
+    const oldPID = deadWorker.process.pid;
 
     // Log the event
-    console.log('worker '+oldPID+' died.');
-    console.log('worker '+newPID+' born.');
+    console.log(`worker ${oldPID} died.`);
+    console.log(`worker ${newPID} born.`);
   });
 
   if (common.config('ping', true)) {
     ping();
     setInterval(ping, common.config('ping').frequency || 60000);
   }
-
 } else {
-  var Device = require('./lib/device');
-  var RedisListener = require('./lib/redis');
+  // eslint-disable-next-line
+  const Device = require('./lib/device');
+  // eslint-disable-next-line
+  const RedisListener = require('./lib/redis');
 
   RedisListener.listen({
     'yodel:subscribe': Device.subscribe,
     'yodel:unsubscribe': Device.unsubscribe,
-    'yodel:notify': Device.notify
+    'yodel:notify': Device.notify,
   });
 
   console.log('Listening to yodel:subscribe, yodel:unsubscribe, and yodel:notify');
 
   if (common.config('apn_feedback', true)) {
+    // eslint-disable-next-line
     require('./lib/feedback');
     console.log('APN Feedback Service monitoring is active');
   }
-}
-
-
-// this will fail if there is nothing in the devices table
-// that may not be what everyone wants
-function ping() {
-  var cfg = common.config('ping', true);
-  https.get(cfg.run_url, function() {
-    return Promise.props({
-      sql: common.knex('devices').max('id as max_id'),
-      rds: common.redis.incrAsync('yodel:ping')
-    }).then(function(testResults) {
-      testResults.sql = testResults.sql[0].max_id;
-
-      for (var k in testResults) {
-        if (isNaN(testResults[k])) {
-          return console.error('Value not a number: '+k);
-        } else if (testResults[k] < 1) {
-          return console.error('Value less than 1: '+k);
-        }
-      }
-
-      https.get(cfg.complete_url);
-    }).catch(common.notifyError);
-  });
 }
--- common.js
+++ common.js
@@ -1,82 +1,92 @@
-var common      = module.exports = {};
-var configCache = {};
-var Promise     = require('bluebird');
-var env         = process.env.NODE_ENV || 'development';
+const common = module.exports = {};
+const configCache = {};
+const Promise = require('bluebird');
+const env = process.env.NODE_ENV || 'development';
 
-var config = common.config = function config(name, allowBlank) {
+const config = common.config = function config(name, allowBlank) {
   if (typeof configCache[name] !== 'undefined') { return configCache[name]; }
 
+  let conf;
   try {
-    var conf = require('./config/'+name);
+    // eslint-disable-next-line
+    conf = require(`./config/${name}`);
   } catch (e) {
-    if (allowBlank) { return configCache[name] = null; }
+    if (allowBlank) {
+      configCache[name] = null;
+      return null;
+    }
     throw e;
   }
 
   conf = conf[env];
   if (!conf) {
     if (allowBlank) {
-      console.error(name+" config not specified for "+env+" environment");
-      return configCache[name] = null;
+      console.error(`${name} config not specified for "+env+" environment`);
+      configCache[name] = null;
+      return null;
     }
-    throw new Error(env+" enviroment not specified for config/"+name);
+    throw new Error(`${env} enviroment not specified for config/${name}`);
   }
 
-  return configCache[name] = conf;
+  configCache[name] = conf;
+  return configCache[name];
 };
 
 common.knex = require('knex')(config('knexfile'));
 
-common.notifyError = function(err) {
+common.notifyError = (err) => {
   console.error(err);
 };
 
-common.logAndThrow = function logAndThrow(err) {
+common.logAndThrow = (err) => {
   console.log(err);
   throw new Error(err);
-}
+};
 
 if (config('sentry', true)) {
-  var raven = require('raven');
-  var ravenClient = new raven.Client(config('sentry').dsn);
+  // eslint-disable-next-line
+  const raven = require('raven');
+  const ravenClient = new raven.Client(config('sentry').dsn);
   ravenClient.patchGlobal();
-  common.notifyError = function(err, callback) {
-    if (!(err instanceof Error)) { err = new Error(err); }
-    ravenClient.captureError(err, callback);
-  }
+  common.notifyError = (err, callback) => {
+    let error = err;
+    if (!(error instanceof Error)) { error = new Error(err); }
+    ravenClient.captureError(error, callback);
+  };
 }
 
-common.publishEvent = function(event, callback) {
+common.publishEvent = (event, callback) => {
   console.log('Event: ', event);
   return callback && callback();
-}
+};
 
-common.newRedisClient = function(configName) {
-  var rConfig = config(configName, true) || {};
+common.newRedisClient = (configName) => {
+  const rConfig = config(configName, true) || {};
   rConfig.port = rConfig.port || 6379;
   rConfig.host = rConfig.host || '127.0.0.1';
 
   // Don't want to overwrite any data in a database for another env
-  if (env == 'test' && typeof rConfig.database === 'undefined') {
+  if (env === 'test' && typeof rConfig.database === 'undefined') {
     rConfig.database = 5;
   }
 
-  var redis = require('redis');
+  // eslint-disable-next-line
+  const redis = require('redis');
   Promise.promisifyAll(redis.RedisClient.prototype);
 
-  var client = redis.createClient(
+  const client = redis.createClient(
     rConfig.port, rConfig.host, rConfig.options);
 
   if (!isNaN(rConfig.database)) { client.select(rConfig.database); }
 
   return client;
-}
+};
 
 common.redis = common.newRedisClient('redis');
 
-if (config('redis_events', true) || env == 'test') {
-  var redis = common.newRedisClient('redis_events');
-  common.publishEvent = function(event, callback) {
-    return redis.publishAsync("yodel:events", JSON.stringify(event)).nodeify(callback);
-  }
+if (config('redis_events', true) || env === 'test') {
+  const redis = common.newRedisClient('redis_events');
+  common.publishEvent = (event, callback) => {
+    return redis.publishAsync('yodel:events', JSON.stringify(event)).nodeify(callback);
+  };
 }
